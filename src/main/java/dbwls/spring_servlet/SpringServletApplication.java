6eef3a141d7b67f748ad55559b71297ee2596611
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-06-15 16:28:37 +0900

    add spring mvc
    spring mvc를 이용한 controller 생성

--- src/main/java/dbwls/spring_servlet/SpringServletApplication.java
+++ src/main/java/dbwls/spring_servlet/SpringServletApplication.java
@@ -3,6 +3,8 @@ package dbwls.spring_servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +14,9 @@ public class SpringServletApplication {
 		SpringApplication.run(SpringServletApplication.class, args);
 	}
 
+//	스프링부탁 자동으로 view Resolver를 등록, application.properties에 등록된 prefix, suffix 사용
+//	@Bean
+//	InternalResourceViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 }
